e76c44bf7bc96e9aace99c9f863aa4f1ffbd49be
Jari <personal@jariketting.com>
2019-05-06 22:48:44 +0200

    FIx nonnull issue

--- app/src/main/java/com/example/orbis/MemoryFragment.java
+++ app/src/main/java/com/example/orbis/MemoryFragment.java
@@ -1,15 +1,17 @@
 package com.example.orbis;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MemoryFragment extends Fragment {
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_memory, container, false);
     }
