6776098c572e002d7b920004ff06f127746c31ca
Jari <personal@jariketting.com>
2019-05-06 22:49:35 +0200

    Remove unused class

--- app/src/main/java/com/example/orbis/MainActivity.java
+++ app/src/main/java/com/example/orbis/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
