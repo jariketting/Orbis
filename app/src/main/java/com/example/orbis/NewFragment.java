bed1237d7db1123597f3bbda1db55c1493acfe2c
Jari <personal@jariketting.com>
2019-05-06 13:13:02 +0200

    Add onClick Listener

--- app/src/main/java/com/example/orbis/NewFragment.java
+++ app/src/main/java/com/example/orbis/NewFragment.java
@@ -7,15 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class NewFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new, container, false);
+        View view = inflater.inflate(R.layout.fragment_new, container, false);
+
+        this.cancelOnClickListener(view);
+
+        return view;
     }
 
-    public void cancelAction(View view) {
+    public void cancelOnClickListener(View view) {
 
+        Button CancelButton = view.findViewById(R.id.buttonCancel);
+
+        CancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
