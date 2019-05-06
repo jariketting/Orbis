3966eb6ea0ceb9f4d4be13159ee90e64522ad606
Jari <personal@jariketting.com>
2019-05-06 12:53:05 +0200

    Remove unused imports

--- app/src/main/java/com/example/orbis/NewFragment.java
+++ app/src/main/java/com/example/orbis/NewFragment.java
@@ -1,5 +1,6 @@
 package com.example.orbis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,6 +14,5 @@ public class NewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_new, container, false);
-
     }
 }
