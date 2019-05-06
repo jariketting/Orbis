e75211a30cd9d3aade0bdaa8ae36a5412bb87dfb
Jari <personal@jariketting.com>
2019-05-06 13:24:05 +0200

    Add button cancel listener with changing fragment

--- app/src/main/java/com/example/orbis/NewFragment.java
+++ app/src/main/java/com/example/orbis/NewFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,18 +17,28 @@ public class NewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_new, container, false);
 
         this.cancelOnClickListener(view);
-
         return view;
     }
 
+    /**
+     * Onclick cancel button listener
+     *
+     * When clicked will go back to map view
+     *
+     * @param view view to get cancel button from
+     */
     public void cancelOnClickListener(View view) {
+        Button CancelButton = view.findViewById(R.id.buttonCancel); //get cancel button by view ID
 
-        Button CancelButton = view.findViewById(R.id.buttonCancel);
-
+        //create listener
         CancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Fragment mapFragment = new MapFragment(); //create fragment
 
+                //prevent errors
+                if (getFragmentManager() != null)
+                    getFragmentManager().beginTransaction().add(R.id.container, mapFragment).commit(); //go back to map fragment
             }
         });
     }
