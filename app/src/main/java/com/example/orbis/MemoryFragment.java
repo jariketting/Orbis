7eec48f8a112a94e32d459a3b14e6b782f732b97
Jari <personal@jariketting.com>
2019-05-20 23:05:03 +0200

    Back button working

--- app/src/main/java/com/example/orbis/MemoryFragment.java
+++ app/src/main/java/com/example/orbis/MemoryFragment.java
@@ -1,5 +1,6 @@
 package com.example.orbis;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,11 +24,26 @@ public class
 MemoryFragment extends Fragment implements OnMapReadyCallback {
     MapView mapView;
     GoogleMap map;
+    Toolbar toolbar;
 
+    @SuppressLint("PrivateResource")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_memory, container, false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+
+        //setup toolbar
+        toolbar.setTitle(R.string.memory_toolbar_title + ": " + "This is a memory");
+        toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getFragmentManager() != null)
+                    getFragmentManager().popBackStack();
+            }
+        });
 
         // Gets the MapView from the XML layout and creates it
         mapView = view.findViewById(R.id.mapView);
