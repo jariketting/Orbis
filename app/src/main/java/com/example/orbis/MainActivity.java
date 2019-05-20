e454504c807d3debff365eaf6d7d116d303742a7
Jari <personal@jariketting.com>
2019-05-21 00:53:22 +0200

    Fix menu item multi pressed issue

--- app/src/main/java/com/example/orbis/MainActivity.java
+++ app/src/main/java/com/example/orbis/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
-    int lastMenuItemId = 0;
+    int menuIdCurrent;
+    int menuIdLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         // StartScreen selection
         getSupportFragmentManager().beginTransaction().replace(R.id.container,          // Hier geef ik aan dat als de app opstart, hij het Kaart Fragment laat zien, als wijze van homescherm
                 new MapFragment()).commit();
+
+        menuIdCurrent = 0;
+        menuIdLast = 0;
     }
 
     /**
@@ -42,14 +48,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToLastFragment() {
+        //TODO fix issue when menu item is already clicked
         getSupportFragmentManager().popBackStack();
 
-
-        Fragment currentFragment = getSupportFragmentManager()
-                .findFragmentById(R.id.content_frame);
-
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.getMenu().getItem(lastMenuItemId).setChecked(true);
+        bottomNav.getMenu().getItem(menuIdLast).setChecked(true);
+
+        menuIdCurrent = menuIdLast;
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =         // Hier wordt geselecteerd op welk fragment er is gedrukt
@@ -58,26 +63,38 @@ public class MainActivity extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                     Fragment selectedFragment = null;
 
+                    int menuId = 0;
+
                     switch (menuItem.getItemId()) {
                         case R.id.map:
                             selectedFragment = new MapFragment();
+                            menuId = 0;
                             break;
                         case R.id.books:
                             //selectedFragment = new DiaryFragment();
                             selectedFragment = new MemoryFragment(); //temp for development purposes
+                            menuId = 1;
                             break;
                         case R.id.add:
                             selectedFragment = new NewFragment();
+                            menuId = 2;
                             break;
                         case R.id.search:
                             selectedFragment = new SearchFragment();
+                            menuId = 3;
                             break;
                         case R.id.account:
                             selectedFragment = new AccountFragment();
+                            menuId = 4;
                             break;
                     }
                     getSupportFragmentManager().beginTransaction().replace(R.id.container,    // Nadat er op eentje is gedrukt wordt deze ge-commit, dus vertoond en hierna begint de loop weer opnieuw
                             selectedFragment).addToBackStack(null).commit();
+
+                    if(menuIdCurrent != menuId)
+                        menuIdLast = menuIdCurrent;
+
+                    menuIdCurrent = menuId;
 
                     return true;
                 }
