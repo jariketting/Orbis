4e9130b92ac6ffc3baecfddffe7470cd04dbf097
Jari <personal@jariketting.com>
2019-05-21 09:32:37 +0200

    Add comments

--- app/src/main/java/com/example/orbis/MainActivity.java
+++ app/src/main/java/com/example/orbis/MainActivity.java
@@ -23,8 +23,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.container,          // Hier geef ik aan dat als de app opstart, hij het Kaart Fragment laat zien, als wijze van homescherm
                 new MapFragment()).commit();
 
-        menuIdCurrent = 0;
-        menuIdLast = 0;
+        //store menu history
+        menuIdCurrent = 0; //current menu item id (defaults to zero / map)
+        menuIdLast = 0; //last menu item id (defaults to zero / map)
     }
 
     /**
@@ -45,13 +46,17 @@ public class MainActivity extends AppCompatActivity {
         nav.setVisibility(View.VISIBLE); //set visibility to visible
     }
 
+    /**
+     * Go to last fragment
+     */
     public void goToLastFragment() {
-        getSupportFragmentManager().popBackStack();
+        getSupportFragmentManager().popBackStack(); //goes to previous fragment stored in the backstack
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.getMenu().getItem(menuIdLast).setChecked(true);
+        //change menu to fragments menu item
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation); //get bottom navigation
+        bottomNav.getMenu().getItem(menuIdLast).setChecked(true); //set previous fragment item checked
 
-        menuIdCurrent = menuIdLast;
+        menuIdCurrent = menuIdLast; //set current menu id to last menu item id
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =         // Hier wordt geselecteerd op welk fragment er is gedrukt
@@ -60,11 +65,13 @@ public class MainActivity extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                     Fragment selectedFragment = null;
 
-                    int menuId = 0;
+                    int menuId = 0; //stores selected menu id, zero is default (map)
 
+                    //prevent current menu item being selected twice
                     if(menuIdCurrent != menuId)
                         return false;
 
+                    //go trough each menu item, then set item id and set selected fragment
                     switch (menuItem.getItemId()) {
                         case R.id.map:
                             selectedFragment = new MapFragment();
@@ -88,12 +95,14 @@ public class MainActivity extends AppCompatActivity {
                             menuId = 4;
                             break;
                     }
+
+                    //set new fragment and add to backstack
                     getSupportFragmentManager().beginTransaction().replace(R.id.container,    // Nadat er op eentje is gedrukt wordt deze ge-commit, dus vertoond en hierna begint de loop weer opnieuw
                             selectedFragment).addToBackStack(null).commit();
 
 
+                    //update menu item id history
                     menuIdLast = menuIdCurrent;
-
                     menuIdCurrent = menuId;
 
                     return true;
