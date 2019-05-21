9d447c79a56eaf821f3b7a4dbd0b705745b11344
Daphne1471 <Daphne1471@users.noreply.github.com>
2019-05-21 11:33:10 +0200

    begin validation

--- app/src/main/java/com/example/orbis/LoginActivity.java
+++ app/src/main/java/com/example/orbis/LoginActivity.java
@@ -1,25 +1,46 @@
 package com.example.orbis;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText email;
+    private EditText password;
+    private int counter = 3;
+    private TextView info;
+    private Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+       email = findViewById(R.id.usernameEmailLogin);
+       password = findViewById(R.id.passwordLogin);
+       info = findViewById(R.id.attemptsText);
+       Login = findViewById(R.id.loginButton);
     }
 
     //Button to go from login screen to the home screen
     //Add a checker
     public void loginButton(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-//        Info = findViewById(R.id.attemptsTextView);
-//        validate(Email.getText().toString(), Password.getText().toString());
-        startActivity(intent);
+        if(email.getText().toString().equals("admin")&&password.getText().toString().equals("1234")) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            //add the counter for login attempts
+//            // add a indication for the user of how many tries they have left
+            counter = counter - 1;
+            info.setText("Attempts left: " + counter);
+            if (counter == 0) {
+                Login.setEnabled(false);
+            }
+        }
     }
 
     //Button to go from login screen to the register screen
