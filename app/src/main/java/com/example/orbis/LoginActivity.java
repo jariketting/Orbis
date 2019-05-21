af5a6ddf7b953f9175b990563fc27334b4762cfe
Daphne1471 <Daphne1471@users.noreply.github.com>
2019-05-21 17:14:06 +0200

    begin validation, layout

--- app/src/main/java/com/example/orbis/LoginActivity.java
+++ app/src/main/java/com/example/orbis/LoginActivity.java
@@ -5,10 +5,13 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText email;
@@ -35,7 +38,8 @@ public class LoginActivity extends AppCompatActivity {
     // program cleans up code by turning == into .equals()
     //&& means and
     public void loginButton(View view) {
-        if(email.getText().toString().equals("admin")&& password.getText().toString().equals("1234")) {
+        if(email.getText().toString().equals("admin@hr.nl")&& password.getText().toString().equals("1234")) {
+//            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){}
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }else{
