08e50e56fb1fd6fcce18bc2ed91b774d703653e3
Dion Rosseels <45227048+DionR10@users.noreply.github.com>
2019-05-22 12:29:25 +0200

    zoekend account toegevoegd

---
+++ app/src/main/java/com/example/orbis/FollowingOtherActivity.java
@@ -0,0 +1,13 @@
+package com.example.orbis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FollowingOtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_following_other);
+    }
+}
