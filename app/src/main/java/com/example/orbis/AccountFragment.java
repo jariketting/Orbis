c7138b041085188644f36156035bd54909c6790d
HydraBubble <nilsvandop@github.com>
2019-06-04 10:39:01 +0200

    Fix

--- app/src/main/java/com/example/orbis/AccountFragment.java
+++ app/src/main/java/com/example/orbis/AccountFragment.java
@@ -4,16 +4,26 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AccountFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        ImageButton opensettingsbutton = (ImageButton)view.findViewById(R.id.opensettingsbutton);
 
+        opensettingsbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.container, new SettingsFragment()).addToBackStack(null).commit();
+            }
+        });
+        return view;
     }
 }
