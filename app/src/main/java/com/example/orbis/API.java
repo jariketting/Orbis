d835e5014f47f8b2be0ce51d99d1b92a411d1be6
Jari <personal@jariketting.com>
2019-06-05 11:28:44 +0200

    Add API Class

---
+++ app/src/main/java/com/example/orbis/API.java
@@ -0,0 +1,43 @@
+package com.example.orbis;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+class API {
+    private static final String TAG = "API";
+    private RequestQueue requestQueue;
+
+    API(Context context) {
+        requestQueue = Volley.newRequestQueue(context);
+    }
+
+    void request(String requestUrl, @Nullable JSONObject jsonBody, final APICallback callback) {
+        String baseUrl = "https://api.jariketting.nl/";
+        String url = baseUrl + requestUrl;
+
+        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                callback.onSuccessResponse(response);
+                Log.i(TAG, response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.getMessage());
+            }
+        });
+
+        requestQueue.add(jsonObject);
+    }
+}
