86c01a65fae279a81625697a00cc3c1bbbe0b806
Jari <personal@jariketting.com>
2019-06-05 11:37:06 +0200

    Add PHPDoc

--- app/src/main/java/com/example/orbis/API.java
+++ app/src/main/java/com/example/orbis/API.java
@@ -13,34 +13,54 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
+/**
+ * API class, connect to the Orbis API.
+ */
 class API {
-    private static final String TAG = "API";
-    private RequestQueue requestQueue;
-    private Context appContext;
+    private static final String TAG = "API"; //stores tag for logging
+    private static RequestQueue requestQueue; //stores request que
+    private Context appContext; //stores app context
 
+    /**
+     *
+     * Initialize API
+     *
+     * @param context App context
+     */
     API(Context context) {
         appContext = context;
 
-        requestQueue = Volley.newRequestQueue(context);
+        //only create request que once
+        if(requestQueue != null)
+            requestQueue = Volley.newRequestQueue(context);
     }
 
+    /**
+     * Make a request to the API
+     *
+     * @param requestUrl url without base and leading slash
+     * @param jsonBody json body data, can be null
+     * @param callback callback function
+     */
     void request(String requestUrl, @Nullable JSONObject jsonBody, final APICallback callback) {
-        String baseUrl = appContext.getString(R.string.API_base_url);
-        String url = baseUrl + requestUrl;
+        String baseUrl = appContext.getString(R.string.API_base_url); //get base url
+        String url = baseUrl + requestUrl; //combine base url and request url
 
+        //Make request
         JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                callback.onSuccessResponse(response);
-                Log.i(TAG, response.toString());
+                callback.onSuccessResponse(response); //callback function to be overwritten
+                Log.i(TAG, response.toString()); //log response to the logger
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, error.getMessage());
+                Log.e(TAG, error.getMessage()); //log error message to logger
             }
         });
 
+        //add to request que
         requestQueue.add(jsonObject);
     }
 }
