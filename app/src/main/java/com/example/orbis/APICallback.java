961dd9fdfe390b98a5cfe1596c543f3e7c8c00b6
Jari <personal@jariketting.com>
2019-06-05 11:31:44 +0200

    Add PHPDoc

--- app/src/main/java/com/example/orbis/APICallback.java
+++ app/src/main/java/com/example/orbis/APICallback.java
@@ -2,6 +2,14 @@ package com.example.orbis;
 
 import org.json.JSONObject;
 
+/**
+ * API Callback function
+ *
+ * To be overwritten with own implementation
+ */
 public interface APICallback {
+    /**
+     * @param response
+     */
     void onSuccessResponse(JSONObject response);
 }
