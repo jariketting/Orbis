acc3abe839e5bbcf57e493e5b5852c116b9950b2
Jari <personal@jariketting.com>
2019-06-06 20:57:47 +0200

    Update ID

--- app/src/main/java/com/example/orbis/DiaryFragment.java
+++ app/src/main/java/com/example/orbis/DiaryFragment.java
@@ -67,7 +67,7 @@ public class DiaryFragment extends Fragment {
 
                 //Pass the ID to the memory
                 Bundle bundle = new Bundle(); //bundle stores stuff we want to give to memory
-                bundle.putInt("id", 2); //the id of the memory
+                bundle.putInt("id", 1); //the id of the memory
                 mFragment.setArguments(bundle); //set the bundle to the arguments of the memory so we can access it from there
 
                 main.goToFragment(mFragment, 1);
