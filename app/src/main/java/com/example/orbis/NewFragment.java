aa300525458dcfc06a653eb17dfb78b135675c08
Jari <personal@jariketting.com>
2019-06-06 22:27:22 +0200

    Remove unused

--- app/src/main/java/com/example/orbis/NewFragment.java
+++ app/src/main/java/com/example/orbis/NewFragment.java
@@ -33,7 +33,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
