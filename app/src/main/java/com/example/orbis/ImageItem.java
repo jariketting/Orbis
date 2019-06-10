44259ce83de7d1d3aad4c97a5b60fa281e03ccce
Jari <personal@jariketting.com>
2019-06-10 15:11:13 +0200

    Add image grid stuff, add picaso

---
+++ app/src/main/java/com/example/orbis/ImageItem.java
@@ -0,0 +1,11 @@
+package com.example.orbis;
+
+public class ImageItem {
+    Integer id;
+    String URI;
+
+    ImageItem(Integer id, String URI) {
+        this.id = id;
+        this.URI = URI;
+    }
+}
