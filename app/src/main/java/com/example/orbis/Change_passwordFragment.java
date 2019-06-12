86134e6994a54292d29f7294c3f679bdc7942a3b
HydraBubble <nilsvandop@github.com>
2019-06-12 12:01:57 +0200

    Change password button and button error fix

--- app/src/main/java/com/example/orbis/Change_passwordFragment.java
+++ app/src/main/java/com/example/orbis/Change_passwordFragment.java
@@ -9,15 +9,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Change_passwordFragment extends Fragment {
     MainActivity main; //store main activity
+    View view;
+    API api;
+
+    Button buttonChange;
+    EditText editTextPassword;
+    EditText editTextPasswordAgain;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+        view = inflater.inflate(R.layout.fragment_change_password, container, false);
         main = ((MainActivity) getActivity());
+        api = new API(main);
+
+        buttonChange = view.findViewById(R.id.buttonChange);
+        editTextPassword = view.findViewById(R.id.editTextPassword);
+        editTextPasswordAgain = view.findViewById(R.id.editTextPasswordAgain);
 
         //toolbar
         Toolbar toolbar = view.findViewById(R.id.toolbarChange);
@@ -30,7 +49,52 @@ public class Change_passwordFragment extends Fragment {
             }
         });
 
+        buttonChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editTextPassword.getText().toString().equals(editTextPasswordAgain.getText().toString()) && editTextPassword.getText().toString().length() > 0) {
+                    updatePassword(editTextPassword.getText().toString());
+                } else {
+                    Toast.makeText(main, R.string.change_password_error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         // Inflate the layout for this fragment
         return view;
+    }
+
+
+
+    private void updatePassword(String newPassword){
+        String url = "user/update/";
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("password", newPassword);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        api.request(url, jsonBody, new APICallback() {
+            @Override
+            public void onSuccessResponse(JSONObject response) {
+                try {
+                    onResult(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void onResult(JSONObject object) throws JSONException {
+        JSONObject error = object.getJSONObject("error");
+        JSONObject data = object.getJSONObject("data");
+
+        if(!error.getBoolean("error")) {
+            getFragmentManager().beginTransaction().replace(R.id.container, new SettingsFragment()).addToBackStack(null).commit();
+        }
     }
 }
